2f3aa81109d01ea2b6e3e26862fc161ff0e23715
abrilaislinnhernandez-lab <abrilaislinnhernandez@gmail.com>
2026-09-03 00:27:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const boton = document.getElementById("botonNota");
-const mensaje = document.getElementById("mensaje");
+window.addEventListener("DOMContentLoaded", function () {
 
-const notas = [
-    "Mentira, no sé qué voy a decir en inglés pero esta es mi perspectiva. ☕",
+    const boton = document.getElementById("botonNota");
+    const mensaje = document.getElementById("mensaje");
 
-    "Le agradezco a tu mamá y a tu papá por haberte educado y hacer de ti lo que eres ahora, deben estar muy orgullosos de ti (porque yo sí).",
+    const notas = [
+        "Mentira, no sé qué voy a decir en inglés pero esta es mi perspectiva. ☕",
 
-    "Sigamos hablando de cincuenta mil cosas sin sentido, it doesn't matter.",
+        "Le agradezco a tu mamá y a tu papá por haberte educado y hacer de ti lo que eres ahora, deben estar muy orgullosos de ti (porque yo sí).",
 
-    "Si no quedó claro o si no me expresé más, en conclusión: es uno de los días más agradables que pueda recordar.",
+        "Sigamos hablando de cincuenta mil cosas sin sentido, it doesn't matter.",
 
-    "¿Tienes alguna duda? Tell me. ✨"
-];
+        "Si no quedó claro o si no me expresé más, en conclusión: es uno de los días más agradables que pueda recordar.",
 
-boton.addEventListener("click", function () {
-    const numero = Math.floor(Math.random() * notas.length);
-    mensaje.textContent = notas[numero];
+        "¿Tienes alguna duda? Tell me. ✨"
+    ];
+
+    boton.addEventListener("click", function () {
+        const numero = Math.floor(Math.random() * notas.length);
+        mensaje.textContent = notas[numero];
+    });
+
 });
