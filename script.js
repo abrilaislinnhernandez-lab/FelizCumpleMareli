fdcdb8967462903e84e46d66911b2e43855ad11d
abrilaislinnhernandez-lab <abrilaislinnhernandez@gmail.com>
2026-09-03 00:16:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-const boton =
-document.getElementById("botonNota");
-
-const mensaje =
-document.getElementById("mensaje");
-
+const boton = document.getElementById("botonNota");
+const mensaje = document.getElementById("mensaje");
 
 const notas = [
+    "Mentira, no sé qué voy a decir en inglés pero esta es mi perspectiva. ☕",
 
-    "Mentira,no se que voy a decir en ingles pero esta es mi perspectiva. ☕",
+    "Le agradezco a tu mamá y a tu papá por haberte educado y hacer de ti lo que eres ahora, deben estar muy orgullosos de ti (porque yo sí).",
 
-    "Le agradezco a tu mamá y a tu papá por haberte educado y hacer de ti lo que eres ahora,deben estar muy orgullosos de ti(porque yo si)",
+    "Sigamos hablando de cincuenta mil cosas sin sentido, it doesn't matter.",
 
-    "Sigamos hablando de cincuenta mil cosas sin sentido,it doesn´t matter(la esquizofrenia esta fuerte)",
+    "Si no quedó claro o si no me expresé más, en conclusión: es uno de los días más agradables que pueda recordar.",
 
-    "Si no quedo claro o si no me exprese más en conclusion:es uno de los dias más agradables que pueda recordar mi super mind",
-
-    "¿Tienes alguna duda?Tell me . ✨"
-
+    "¿Tienes alguna duda? Tell me. ✨"
 ];
+
+boton.addEventListener("click", function () {
+    const numero = Math.floor(Math.random() * notas.length);
+    mensaje.textContent = notas[numero];
+});
