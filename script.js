4291b759c82c38bafab745f53a28221957f166b2
abrilaislinnhernandez-lab <abrilaislinnhernandez@gmail.com>
2026-09-02 23:58:28 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,20 @@
-// ===========================
-// BOTÓN "ABRIR MI REGALO"
-// ===========================
+const boton =
+document.getElementById("botonNota");
 
-const inicio = document.getElementById("startBtn");
-const loader = document.getElementById("loader");
-const contenido = document.getElementById("contenido");
-
-inicio.addEventListener("click", () => {
-
-    loader.style.display = "none";
-    contenido.classList.remove("hidden");
-
-});
+const mensaje =
+document.getElementById("mensaje");
 
 
-// ===========================
-// CONTADOR DEL CUMPLEAÑOS
-// ===========================
+const notas = [
 
-const inicioCumple = new Date(2026, 6, 23, 0, 0, 0); // 23 julio 2026 00:00:00
-const finCumple = new Date(2026, 6, 24, 0, 0, 0);    // 24 julio 2026 00:00:00
+    "Mentira,no se que voy a decir en ingles pero esta es mi perspectiva. ☕",
 
-function actualizarContador() {
+    "Le agradezco a tu mamá y a tu papá por haberte educado y hacer de ti lo que eres ahora,deben estar muy orgullosos de ti(porque yo si)",
 
-    const ahora = new Date();
+    "Sigamos hablando de cincuenta mil cosas sin sentido,it doesn´t matter(la esquizofrenia esta fuerte)",
 
-    // Antes del cumpleaños
-    if (ahora < inicioCumple) {
+    "Si no quedo claro o si no me exprese más en conclusion:es uno de los dias más agradables que pueda recordar mi super mind",
 
-        document.getElementById("dias").textContent = "00";
-        document.getElementById("horas").textContent = "00";
-        document.getElementById("minutos").textContent = "00";
-        document.getElementById("segundos").textContent = "00";
+    "¿Tienes alguna duda?Tell me . ✨"
 
-        return;
-    }
-
-    // Después del cumpleaños
-    if (ahora >= finCumple) {
-
-        document.getElementById("dias").textContent = "01";
-        document.getElementById("horas").textContent = "00";
-        document.getElementById("minutos").textContent = "00";
-        document.getElementById("segundos").textContent = "00";
-
-        return;
-    }
-
-    const diferencia = ahora - inicioCumple;
-
-    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-
-    const horas = Math.floor(
-        (diferencia % (1000 * 60 * 60 * 24)) /
-        (1000 * 60 * 60)
-    );
-
-    const minutos = Math.floor(
-        (diferencia % (1000 * 60 * 60)) /
-        (1000 * 60)
-    );
-
-    const segundos = Math.floor(
-        (diferencia % (1000 * 60)) /
-        1000
-    );
-
-    document.getElementById("dias").textContent = dias.toString().padStart(2, "0");
-    document.getElementById("horas").textContent = horas.toString().padStart(2, "0");
-    document.getElementById("minutos").textContent = minutos.toString().padStart(2, "0");
-    document.getElementById("segundos").textContent = segundos.toString().padStart(2, "0");
-
-}
-
-actualizarContador();
-
-setInterval(actualizarContador, 1000);
+];
